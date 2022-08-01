70a9b704f71c8c6fac5674b778e66943903c15df
Diego <diegocdsantos@gmail.com>
2022-08-01 19:16:07 -0300

    initial commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTranferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTranferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
